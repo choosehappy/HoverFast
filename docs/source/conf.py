b3cac0b097314824fee2b7575a0525c25c5c219c
Petros Liakopoulos <petros.liakopoulos@unige.ch>
2024-10-01 13:28:10 +0200

    correct version on readthedocs

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -9,8 +9,8 @@
 project = 'HoverFast'
 copyright = '2024, Petros Liakopoulos, Julien Massonnet, Andrew Janowczyk'
 author = 'Petros Liakopoulos, Julien Massonnet, Andrew Janowczyk'
-release = '1.0.0'
-version = '1.0.0'
+release = '1.1.0'
+version = '1.1.0'
 
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
